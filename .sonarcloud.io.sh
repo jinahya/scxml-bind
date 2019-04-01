5688d1c3de92f216cf27c44e294d9340e7ba3ce6
Jin Kwon <onacit@wemakeprice.com>
2019-04-01 12:22:39 +0900

    Remove sonar script

--- .sonarcloud.io.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-mvn sonar:sonar \
-  -Dsonar.projectKey=jinahya_scxml-bind \
-  -Dsonar.organization=jinahya-github \
-  -Dsonar.host.url=https://sonarcloud.io \
-  -Dsonar.login=d9bf5bfc55c72888f6f31a4f63087875d29987ed
